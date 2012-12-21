681773da87d6430923727d20f1ebdbc79b77c49d
Julien Viet <julien@julienviet.com>
2012-12-21 14:19:33 +0100

    Breadcrumbs

---
+++ src/main/java/gatein/breadcrumbs/Controller.java
@@ -0,0 +1,40 @@
+package gatein.breadcrumbs;
+
+import juzu.Path;
+import juzu.Response;
+import juzu.View;
+import juzu.template.Template;
+import org.gatein.api.PortalRequest;
+import org.gatein.api.navigation.Node;
+
+import javax.inject.Inject;
+import java.util.LinkedHashMap;
+
+/** @author <a href="mailto:julien.viet@exoplatform.com">Julien Viet</a> */
+public class Controller {
+
+  @Inject
+  PortalRequest request;
+
+  @Inject
+  @Path("nav.gtmpl")
+  Template nav;
+
+  @View
+  public Response.Render index() {
+    LinkedHashMap<String, String> nodes = build(request.getNode());
+    return nav.with().set("nodes", nodes).render();
+  }
+
+  private LinkedHashMap<String, String> build(Node current) {
+    Node parent = current.getParent();
+    LinkedHashMap<String, String> nodes;
+    if (parent != null) {
+      nodes = build(parent);
+    } else {
+      nodes = new LinkedHashMap<String, String>();
+    }
+    nodes.put(current.resolveDisplayName(), current.resolveURI().toString());
+    return nodes;
+  }
+}
